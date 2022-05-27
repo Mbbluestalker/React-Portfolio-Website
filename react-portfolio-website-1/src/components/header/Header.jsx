df14f258c0d3f8e62682a5360ce4fa3fed7cbacf
Akorede olawole <akoredeolawole@gmail.com>
2022-05-27 02:18:48 +0100

    created all components needed for the project

---
+++ react-portfolio-website-1/src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm </h5>
+        <h1>MB</h1>
+        <h5 className="text-light">FullStack Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
