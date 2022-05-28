0de94c14130ef453a33e0f85f1d539e100fbc797
Akorede olawole <akoredeolawole@gmail.com>
2022-05-28 02:46:31 +0100

    added new features for services and portfolio

--- react-portfolio-website-1/src/components/about/About.jsx
+++ react-portfolio-website-1/src/components/about/About.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './about.css'
 import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
 
 const About = () => {
   return (
@@ -16,7 +20,29 @@ const About = () => {
         </div>
 
         <div className="about__content">
+            <div className="about__cards">
+              <article className='about__card'>
+                <FaAward className='about__icon' />
+                <h5>Experience</h5>
+                <small>3+ Years Working Experience</small>
+              </article>
 
+              <article className='about__card'>
+                <FiUsers className='about__icon' />
+                <h5>Clients</h5>
+                <small>200+ Worldwide</small>
+              </article>
+
+              <article className='about__card'>
+                <VscFolderLibrary className='about__icon' />
+                <h5>Projects</h5>
+                <small>80+ Completed</small>
+              </article>
+            </div>
+
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam illo quia similique adipisci dolores sit ab natus architecto nobis cum asperiores libero, hic eaque harum reiciendis aliquid laborum provident ratione?</p>
+
+            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
       </div>
     </section>
--- react-portfolio-website-1/src/components/experience/Experience.jsx
+++ react-portfolio-website-1/src/components/experience/Experience.jsx
@@ -1,10 +1,114 @@
 import React from 'react'
 import './experience.css'
-
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>the Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>FrontEnd Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>JAVASCRIPT</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+              
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>BOOTSTRAP</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+                
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>TAILWIND CSS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>REACT</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+
+        <div className="experience__backend">
+        <h3>BackEnd Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>DJANGO</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>POSTGRESQL</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>PYTHON</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>MYSQL</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>DJANGO REST FRAMEWORK</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
