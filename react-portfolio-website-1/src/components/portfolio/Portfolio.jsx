0a3ad50cf480c1ccaa861b62aacc24a58ebfc1ff
Akorede Olawole <akoredeolawole@gmail.com>
2024-04-04 09:24:59 +0100

    added some new projects

--- react-portfolio-website-1/src/components/portfolio/Portfolio.jsx
+++ react-portfolio-website-1/src/components/portfolio/Portfolio.jsx
@@ -6,6 +6,9 @@ import IMG3 from "../../assets/githubfinder.png";
 import IMG4 from "../../assets/portfolio4.jpg";
 import IMG5 from "../../assets/africanproverb.png";
 import IMG6 from "../../assets/portfolio6.jpg";
+import KoKoCore from "../../assets/koko_core.png";
+import KokoPatient from "../../assets/koko_patient.png";
+import RealCousins from "../../assets/real_cousins.png";
 
 const data = [
   {
@@ -13,7 +16,7 @@ const data = [
     image: IMG5,
     title: "African Proverb - React / Next JS",
     github: "https://github.com/",
-    demo: "http://africanproverbs.com/",
+    demo: "http://dev.africanproverbs.com/",
   },
 
   {
@@ -39,21 +42,28 @@ const data = [
     demo: "https://gitfinder-context.netlify.app/",
   },
 
-  // {
-  //   id: 5,
-  //   image: IMG5,
-  //   title: 'Orion UI kit - Charts templates & infographics in Figma',
-  //   github: 'https://github.com/',
-  //   demo: 'https://dribbble.com/shots/18308150-Orion-UI-kit-Charts-templates-infographics-in-Figma?utm_source=Clipboard_Shot&utm_campaign=Alien_pixels&utm_content=Orion%20UI%20kit%20-%20Charts%20templates%20%26%20infographics%20in%20Figma&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=Alien_pixels&utm_content=Orion%20UI%20kit%20-%20Charts%20templates%20%26%20infographics%20in%20Figma&utm_medium=Social_Share'
-  // },
+  {
+    id: 5,
+    image: KoKoCore,
+    title: "Koko Core - React / Next JS",
+    github: "https://github.com/Mbbluestalker",
+    demo: 'https://kokocore.calmglobal.com/'
+  },
 
-  // {
-  //   id: 6,
-  //   image: IMG6,
-  //   title: 'Orion UI kit - Charts templates & infographics in Figma',
-  //   github: 'https://github.com/',
-  //   demo: 'https://dribbble.com/shots/18308230-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps?utm_source=Clipboard_Shot&utm_campaign=Alien_pixels&utm_content=Eclipse%20-%20Figma%20dashboard%20UI%20kit%20for%20data%20design%20web%20apps&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=Alien_pixels&utm_content=Eclipse%20-%20Figma%20dashboard%20UI%20kit%20for%20data%20design%20web%20apps&utm_medium=Social_Share'
-  // }
+  {
+    id: 6,
+    image: KokoPatient,
+    title: "Koko Patient - React / Next JS",
+    github: "https://github.com/Mbbluestalker",
+    demo: 'https://kokopatient.calmglobal.com/'
+  },
+  {
+    id: 7,
+    image: RealCousins,
+    title: "Real Cousins - React / Next JS",
+    github: "https://github.com/Mbbluestalker",
+    demo: 'https://dev.realcousins.com/'
+  }
 ];
 
 const Portfolio = () => {
